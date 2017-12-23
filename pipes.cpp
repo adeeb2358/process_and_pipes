a9556e5e2474fd9c77c6f0830a02709ed449e2ee
adeeb2358 <adeeb2358@gmail.com>
2017-12-24 01:08:05 +0530

    single command pareser aded in pipe_sehll

--- pipes.cpp
+++ pipes.cpp
@@ -45,9 +45,11 @@ int prompt_and_parse(char** up_stream,char **down_stream){
 	}
 	
 	int compare_result = strcmp(one_line,"END");
+	//printf("%d compare result \n",compare_result);
 	if(compare_result == 10){
 		return -1;
 	}
+	
 	
 	/*
 		strtok initially take first piece
@@ -58,37 +60,53 @@ int prompt_and_parse(char** up_stream,char **down_stream){
 	while(*up_stream = strtok(NULL,"\t")){
 		if(strcmp(*up_stream," |") == 0){
 			*up_stream = NULL;
-		}
-	}
-	
-	/*one_piece = strtok(one_line," \t");
-	printf("%s\n",one_piece);*/
-	/*one_piece = strtok(one_line," \t");
-	printf("%s\n",one_piece);
-	one_area = strtok(NULL," \t");
-	one_area = strtok(NULL," \t");
-	one_area = strtok(NULL," \t");
-	one_area = strtok(NULL," \t");*/
-	
-	/**u++  = strtok(one_line,"\t");
-	while(*u = strtok(NULL,"\t")){
-		if(strcmp(*u,"|") == 0){
-			*u = NULL;
-			while(*d++ = strtok(NULL,"\t")){
+			while(*down_stream++ = strtok(NULL,"\t")){
 				return 1;
 			}
 		}
-		u++;
-	}*/
+		*up_stream ++;
+	}
 	
 	return 1;
 }  
 
+void sanitize_string(char* good_string,char* one_string){
+	char bad_chars[] = {'\n','$','\''};
+	
+	for(int i = 0,j = 0; i < 20; i++){
+		if((one_string[i] != bad_chars[0]) && 
+		  	(one_string[i] != bad_chars[1]) &&
+			(one_string[i] != bad_chars[2])
+			){
+			
+			good_string[j] = one_string[i];
+			j++;
+		}
+	}
+}
+
 void pipe_shell(){
 	int pipe_descriptor[2];
 	char *up_stream[20], *down_stream[20];
-	while(prompt_and_parse(up_stream,down_stream) > 0){
-							
+	while(1){
+		int parse_result = prompt_and_parse(up_stream,down_stream);
+		if(parse_result == -1){
+			exit(0);
+		}
+		if(down_stream[0] == NULL){
+			/*only one command is given to the sample terminal*/
+			if(fork() == 0){/*child process*/
+				char good_command[20];
+				sanitize_string(good_command,up_stream[0]);
+				up_stream[0] = good_command;
+				
+				execvp(up_stream[0],up_stream);
+				printf("%s :not found\n",good_command);
+				exit(1);
+			}else{
+				wait(0);
+			}
+		}						
 	}
 	return;
 }
--- pipes.h
+++ pipes.h
@@ -3,4 +3,5 @@
 int prompt_and_parse(char** upstream,char **down_stream);
 void pipe_function();
 void pipe_shell();
+void sanitize_string(char* good_string,char* one_string);
 #endif // PIPES_H
